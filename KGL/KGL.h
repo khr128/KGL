65746f527d1f6ac35c443d26227072b3a174241c
Dmitri Khrebtukov <khr128@gmail.com>
2013-08-16 11:12:26 -0700

    Initial Commit

---
+++ KGL/KGL.h
@@ -0,0 +1,13 @@
+//
+//  KGL.h
+//  KGL
+//
+//  Created by khr on 8/16/13.
+//  Copyright (c) 2013 khr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KGL : NSObject
+
+@end
