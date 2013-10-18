d3385682235ec26df8472adb11a551462f2264aa
Dmitri Khrebtukov <khr128@gmail.com>
2013-10-18 06:23:16 -0700

    Add général purpose KGL class

---
+++ KGL/KGL.h
@@ -0,0 +1,13 @@
+//
+//  KGL.h
+//  KGL
+//
+//  Created by khr on 10/17/13.
+//  Copyright (c) 2013 khr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KGL : NSObject
++ (void)logVersionsAndCapabilities;
+@end
---
+++ KGL/KGL.m
@@ -0,0 +1,25 @@
+//
+//  KGL.m
+//  KGL
+//
+//  Created by khr on 10/17/13.
+//  Copyright (c) 2013 khr. All rights reserved.
+//
+
+#import "KGL.h"
+
+@implementation KGL
+
++ (void)logVersionsAndCapabilities {
+  const char *verstr = (const char *) glGetString(GL_VERSION);
+  NSLog(@"OpenGL version: %s", verstr);
+  
+  const char *verstrSL = (const char *) glGetString(GL_SHADING_LANGUAGE_VERSION);
+  NSLog(@"Shading language version: %s", verstrSL);
+  
+  GLint maxTextureSize;
+  glGetIntegerv(GL_MAX_TEXTURE_SIZE, &maxTextureSize);
+  NSLog(@"Max texture size: %d", maxTextureSize);
+}
+
+@end
